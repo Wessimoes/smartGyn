4b59239f4fabbca2693b73e5bf62e180bc1f6f34
Wesley Simões <wesleylucas13@gmail.com>
2017-11-29 19:05:27 -0200

    update subtitle

--- src/layout/components/home/PanelHome.jsx
+++ src/layout/components/home/PanelHome.jsx
@@ -16,7 +16,7 @@ export default class PanelHome extends React.Component {
                 <Row>
                     <Col12>
                         <Card style={{marginBottom: 8}}>
-                            <CardTitle title="Smart Gym" subtitle="slogan massa qui" />
+                            <CardTitle title="Smart Gym" subtitle="Sistema de gestão de fichas" />
                             <CardText>
                                 Área administrativa do sistema, para a gestão das fichas dos alunos..
                             </CardText>
